125706c51688562fae966bd54d9ebd985cdd96d2
Julian <cusco2891@gmail.com>
2022-09-08 02:27:38 -0400

    workingng on toolbar// middle row

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3e778c0f6f30210"; }
+/******/ 		__webpack_require__.h = function() { return "fc23f9da4e573f1e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
