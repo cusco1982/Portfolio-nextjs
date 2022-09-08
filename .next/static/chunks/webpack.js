e68f28789677bc09b1d50eac1c778a5b076cf37d
Julian <cusco2891@gmail.com>
2022-09-08 00:03:12 -0400

    footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84d16e0e698b7873"; }
+/******/ 		__webpack_require__.h = function() { return "b3e778c0f6f30210"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
