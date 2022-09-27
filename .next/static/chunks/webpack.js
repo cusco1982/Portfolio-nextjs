5fd26d7c16a0fed52bb0b1a99028384de2bac99f
Julian <cusco2891@gmail.com>
2022-09-26 23:53:04 -0400

    css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "48b1ead717386cae"; }
+/******/ 		__webpack_require__.h = function() { return "1f92e08b589804a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
