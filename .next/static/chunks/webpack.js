2b71c1231632a05cfeadd9108ba8e32165dd90b5
Julian <cusco2891@gmail.com>
2022-10-06 16:59:36 -0400

    example projects/landing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "69d7ac14d0e1dd66"; }
+/******/ 		__webpack_require__.h = function() { return "cda38c24ba37b7d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.09a7531bfcfa9a52.hot-update.js
+++ .next/static/webpack/webpack.09a7531bfcfa9a52.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f394856da60376d1"; }
+/******/ 	__webpack_require__.h = function() { return "cc2ec727ff398cea"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
