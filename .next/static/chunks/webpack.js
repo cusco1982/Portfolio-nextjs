a79e566d03bc465d883008831b9348435ad2173d
Julian <cusco2891@gmail.com>
2022-10-19 22:54:23 -0400

    footer finished minus// contact info

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8052707502c78619"; }
+/******/ 		__webpack_require__.h = function() { return "dde41216fc089255"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
